2af918d06bde22f6354d5bcd1468e48dee54a0bd
daurarapp-tkp <daura.rapp@gmail.com>
2026-09-19 13:27:52 +0545

    Add Supabase public configuration for Daura ordering

---
+++ supabase-config.js
@@ -0,0 +1,3 @@
+window.DAURA_SUPABASE_URL='https://xcmyieefdpaaipngphym.supabase.co';
+window.DAURA_SUPABASE_KEY='sb_publishable_iraP8sepBprMFD2cildkrQ_TTXOqoXq';
+window.DAURA_WHATSAPP='9779744444442';
